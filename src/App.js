37024688acc27a04aa3861d20e118aa864c5a062
Harsha2k4 <saikumar.21bce9856@vitapstudent.ac.in>
2023-10-15 00:00:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useId, useState } from "react";
+import "./styles.css";
+
+
+
+        
+        
+
+export default function App() {
+  var currentYear = 2023;
+  const [birthYear, setbirthYear] = useState(0);
+  const [age, setAge] = useState(0);
+  return (<div className="App">
+    <h1>Age Calculator</h1>
+      
+      <input onChange={(e) => inputChangeHandler(e.target.value)} />
+      <div >
+        {" "}
+        <button onClick={clickHandler}>Submit</button>
+      </div>
+
+      <div>The age is: {age}</div> 
+    </div>);
+
+
+  function clickHandler() {
+    var theAge = currentYear - birthYear;
+    setAge(theAge);
+  }
+
+  function inputChangeHandler(e) {
+    setbirthYear(e);
+  }
+
+}
+
+
